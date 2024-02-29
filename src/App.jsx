be74d08ef278ee7d85c47dca3032fd54a1fd350b
rama-bou <maxiramiro2017@gmail.com>
2024-02-28 21:43:26 -0300

    icono de react eliminado, limites de clicks agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component2 } from './components/Component2';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component2 } from "./components/Component2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Presiona el boton!</h1>
-        <Component2/>
+        <Component2 />
       </header>
     </div>
   );
--- src/components/Component2.jsx
+++ src/components/Component2.jsx
@@ -1,17 +1,23 @@
-import React, {useState} from 'react';
-import './Component2.css';
+import React, { useState } from "react";
+import "./Component2.css";
 
 export const Component2 = () => {
-    let [cantidad,Presionado] = useState(0)
+  const [cantidad, setCantidad] = useState(0);
 
-    const sumaClick = () => {
-        Presionado(cantidad + 1)
-    }
+  const sumaClick = () => {
+    setCantidad(cantidad + 1);
+  };
 
-    return (
+  return (
     <div>
-        <h2>Presionaste el boton: {cantidad} veces</h2>
-        <button id='botoncito' onClick={sumaClick}><strong>Presioname</strong></button>
+      <h2>Presionaste el botón: {cantidad} veces</h2>
+      {cantidad < 1000000 ? (
+        <button id="botoncito" onClick={sumaClick}>
+          <strong>Presioname</strong>
+        </button>
+      ) : (
+        <p>Llegaste al 1 millón de clics! POR FAVOR, ANDA A TOMAR AIRE</p>
+      )}
     </div>
-  )
-}
+  );
+};
